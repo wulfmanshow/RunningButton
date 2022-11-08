568d5adc947dc619e927e7ef349f08a21e5f0fb8
Nikita-Poshtaruk <fanadeich@gmail.com>
2022-11-08 23:39:49 +0300

    Добавьте файлы проекта.

---
+++ WinFormsApp6/Form1.Designer.cs
@@ -0,0 +1,91 @@
+﻿
+namespace WinFormsApp6
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.yesButton = new System.Windows.Forms.Button();
+            this.noButton = new System.Windows.Forms.Button();
+            this.question1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // yesButton
+            // 
+            this.yesButton.Location = new System.Drawing.Point(213, 308);
+            this.yesButton.Name = "yesButton";
+            this.yesButton.Size = new System.Drawing.Size(75, 23);
+            this.yesButton.TabIndex = 0;
+            this.yesButton.Text = "Так";
+            this.yesButton.UseVisualStyleBackColor = true;
+            this.yesButton.Click += new System.EventHandler(this.yesButton_Click);
+            // 
+            // noButton
+            // 
+            this.noButton.Location = new System.Drawing.Point(519, 308);
+            this.noButton.Name = "noButton";
+            this.noButton.Size = new System.Drawing.Size(75, 23);
+            this.noButton.TabIndex = 1;
+            this.noButton.Text = "Ні";
+            this.noButton.UseVisualStyleBackColor = true;
+            this.noButton.Click += new System.EventHandler(this.noButton_Click);
+            this.noButton.MouseMove += new System.Windows.Forms.MouseEventHandler(this.noButton_MouseMove);
+            // 
+            // question1
+            // 
+            this.question1.AutoSize = true;
+            this.question1.Font = new System.Drawing.Font("Segoe UI", 30F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.question1.Location = new System.Drawing.Point(56, 120);
+            this.question1.Name = "question1";
+            this.question1.Size = new System.Drawing.Size(701, 54);
+            this.question1.TabIndex = 2;
+            this.question1.Text = "Ви задовільнені заробітною платою?";
+            this.question1.Click += new System.EventHandler(this.question1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(819, 481);
+            this.Controls.Add(this.question1);
+            this.Controls.Add(this.noButton);
+            this.Controls.Add(this.yesButton);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button yesButton;
+        private System.Windows.Forms.Button noButton;
+        private System.Windows.Forms.Label question1;
+    }
+}
+
---
+++ WinFormsApp6/Form1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WinFormsApp6
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void noButton_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Шкода :(", "начальство");
+        }
+
+        private void yesButton_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("ми знали що вас задовільняє ваша зарплата", "начальство");
+        }
+
+        private void noButton_MouseMove(object sender, MouseEventArgs e)
+        {
+            Text = $"{noButton.Top} {noButton.Left}---{e.X}  {e.Y}";
+            noButton.Top -= e.Y;
+            noButton.Left += e.X;
+            if (noButton.Top < 200 || noButton.Top > 350)
+            {
+                noButton.Top = 300;
+            }
+            if (noButton.Left < -80 || noButton.Left > 450)
+            {
+                noButton.Left = 308;
+            }
+        }
+
+        private void question1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
